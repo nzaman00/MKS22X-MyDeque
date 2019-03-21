129569c23abec1430f27d01a82a8f15106611105
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-21 09:49:25 -0400

    constructor

--- MyDeque.java
+++ MyDeque.java
@@ -1,15 +1,22 @@
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
-
-  public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(E element){ }
-  public E removeLast(E element){ }
-  public E getFirst(E element){ }
-  public E getLast(E element){ }
+  
+  @SuppressWarnings("unchecked")
+  public MyDeque(){  
+    data = (E[]) new object [10];
+  }
+  
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity){
+    data = (E[]) new object [initialCapacity];
+  }
+  //public int size(){ }
+  //public String toString(){ }
+  //public void addFirst(E element){ }
+  //public void addLast(E element){ }
+ // public E removeFirst(E element){ }
+ // public E removeLast(E element){ }
+  //public E getFirst(E element){ }
+  //public E getLast(E element){ }
 }
