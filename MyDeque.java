ad673a22d5fb07977786f9a6863d3e9e985a5c1d
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-21 09:41:21 -0400

    Update MyDeque.java

--- MyDeque.java
+++ MyDeque.java
@@ -1,1 +1,15 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
 
+  public MyDeque(){  }
+  public MyDeque(int initialCapacity){  }
+  public int size(){ }
+  public String toString(){ }
+  public void addFirst(E element){ }
+  public void addLast(E element){ }
+  public E removeFirst(E element){ }
+  public E removeLast(E element){ }
+  public E getFirst(E element){ }
+  public E getLast(E element){ }
+}
