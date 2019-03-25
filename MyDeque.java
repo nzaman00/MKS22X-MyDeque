24cbe7050e026f8d37011ffa4cdb64ed1f97d018
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-25 19:08:46 -0400

    Update MyDeque.java

--- MyDeque.java
+++ MyDeque.java
@@ -1,36 +1,32 @@
+import java.util.*;
+import java.io.*;
+
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
   @SuppressWarnings("unchecked")
   public MyDeque(){
-    data = (E[]) new Object [10];
-    size = 0;
+    data = (E[])new Object[10];
   }
 
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
-    data = (E[]) new Object [initialCapacity];
-    size = 0;
+    data = (E[])new Object[initialCapacity];
   }
+
   public int size(){
     return size;
-    }
-  //public String toString(){ }
-  public void addFirst(E element){
-    if (size == 0){
-      size = 1;
-      start = 0;
-      end = 0;
-      data[start]= element;
-    }
-    size++;
-    end++;
-    data[start]= element;
   }
-  //public void addLast(E element){ }
- // public E removeFirst(E element){ }
- // public E removeLast(E element){ }
-  //public E getFirst(E element){ }
-  //public E getLast(E element){ }
+
+  public String toString(){
+    String output = "{";
+    int index = start;
+    for (int i = 0; i < size; i++){
+      output += data[index % data.length] + " ";
+      index++;
+    }
+    return output + "}";
+  }
+  
 }
