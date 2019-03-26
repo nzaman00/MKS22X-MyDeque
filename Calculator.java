60f102885ac614b63a24799b07fee8c05e1e750d
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-26 10:11:59 -0400

    so far so good

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,8 @@ public class Calculator{
   public static double eval(String s){
     String[] arg = s.split(" ");
     MyDeque<Object> E = new MyDeque<Object>(arg.length);
+    //loops through and adds all the args to the MyDeque
+    // leaves operators as strings, turns numbers into double
     for(int i = 0; i < arg.length; i++){
       if(!arg[i].equals("+") && !arg[i].equals("-") && !arg[i].equals("*") && !arg[i].equals("/") && !arg[i].equals("%")){
         E.addLast(Double.parseDouble(arg[i]));
@@ -10,11 +12,17 @@ public class Calculator{
         E.addLast(arg[i]);
       }
     }
-    System.out.println(E);
+    //System.out.println(E);
 
   return 1.0;
+  //look through the deque, if it's operator, perform it on the two last numbers before said operator
+  
   }
   public static void main(String[] args) {
     eval("10 2.0 +");
+    eval("11 3 - 4 + 2.5 *");
+    eval("8 2 + 99 9 - * 2 + 9 -");
+    eval("1 2 3 4 5 + * - -") ;
+
   }
 }
