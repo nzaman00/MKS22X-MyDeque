21464255fed3bad33cc7557cba5e5b3096a3c747
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-26 10:01:11 -0400

    adds args in string to mydeque

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,20 @@
 public class Calculator{
   public static double eval(String s){
-  
+    String[] arg = s.split(" ");
+    MyDeque<Object> E = new MyDeque<Object>(arg.length);
+    for(int i = 0; i < arg.length; i++){
+      if(!arg[i].equals("+") && !arg[i].equals("-") && !arg[i].equals("*") && !arg[i].equals("/") && !arg[i].equals("%")){
+        E.addLast(Double.parseDouble(arg[i]));
+      }
+      else{
+        E.addLast(arg[i]);
+      }
+    }
+    System.out.println(E);
+
+  return 1.0;
+  }
+  public static void main(String[] args) {
+    eval("10 2.0 +");
   }
 }
